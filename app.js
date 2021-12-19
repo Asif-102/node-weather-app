f5af5399abfc0bb8804929e79838f9833fede463
Asif-102 <aiqbal517@gmail.com>
2021-12-20 03:42:01 +0600

    makes code more cleaner

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 geoCode('new york',(error, data)=>{
-    console.log(data)
+    console.log('Error', error)
+    console.log('Data', data)
 })
 
 forecast('23.784506','90.403409',(error, data)=>{
-    console.log(data)
+    console.log('Error', error)
+    console.log('Data', data)
 })
 
